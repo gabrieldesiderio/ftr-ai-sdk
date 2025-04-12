4718f48cdcfa33a82389e07958bbf40d319ffb4f
Gabriel Desidério <gabrieldesd7@gmail.com>
2025-04-12 14:08:54 -0300

    setup chat and prompt

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,13 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
-import { generateText } from "ai";
+import { generateObject, generateText, streamText, tool } from "ai";
 import { openrouter } from "@/app/ai/open-router";
+import { z } from "zod";
 
-export async function GET(request: NextRequest) {
-  const result = await generateText({
-    model: openrouter.chat('openai/chatgpt-4o-latest'),
-    prompt: 'Traduza "Hello World" para português!',
-    system: 'Você é uma AI especializada em tradução. Sempre retore da maneira mais sucinta possível.'
+export async function POST(request: NextRequest) {
+  const { messages } = await request.json()
+
+  const result = streamText({
+    model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+    tools: {
+      profileAndURLs: tool({
+        description: 'Essa ferramenta serve para buscar dados do perfil de um usuário no Github, ou acessar URLs da API para outras informações de um usuári como lista de organizações, repositórios, eventos, seguidores, seguindo, e etc.',
+        parameters: z.object({
+          username: z.string().describe('Username do usuário no github')
+        }),
+        execute: async ({ username }) => {
+          const response = await fetch(`https://api.github.com/users/${username}`)
+          const data = await response.json()
+
+          return JSON.stringify(data)
+        }
+      }),
+
+      fetchHttp: tool({
+        description: 'Essa ferramenta serve para realizar uma requisição HTTP em uma URL especificada e acessar sua resposta',
+        parameters: z.object({
+          url: z.string().url().describe('URL a ser requisistada')
+        }),
+        execute: async ({ url }) => {
+          const response = await fetch(url)
+          const data = await response.text()
+
+          return data
+        }
+      })
+    },
+    messages,
+    maxSteps: 5,
+    system: `
+      Sempre responda em markdown sem aspas no inicio ou fim da mensagem.
+    `,
+    onStepFinish({ toolResults }) {
+      console.log(toolResults)
+    }
   })
 
-  return NextResponse.json({ message: result.text })
+  return result.toDataStreamResponse()
 }
